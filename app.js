8613ddc770673373e8925c215b0a23f2a6b1bfee
Vishal Sahgal <111181510+thevishalsahgal@users.noreply.github.com>
2026-01-04 20:49:23 +0530

    Episode 1

---
+++ app.js
@@ -0,0 +1,30 @@
+/* <div id="parent">
+    <div id="child">
+        <h1>Namaste React</h1>
+        <h1>Namaste React</h1>
+    </div>
+    <div id="child2">
+        <h1>Namaste React</h1>
+        <h1>Namaste React</h1>
+    </div>
+</div> */
+
+
+
+
+const heading = React.createElement(
+        "div", {
+            id: "parent"
+        }, [
+            React.createElement("div", {
+                    id: "child"
+                },
+                [React.createElement("h1", {}, "Namaste React"), React.createElement("h2", {}, "Namaste React")], React.createElement("div", {
+                        id: "child2"
+                    },
+                    [React.createElement("h1", {}, "Namaste React"), React.createElement("h2", {}, "Namaste React")]))
+        ])
+
+
+const root = ReactDOM.createRoot(document.getElementById("root")); 
+root.render(heading);
